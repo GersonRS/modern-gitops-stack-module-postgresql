8d1329510fe0113945289cd246669171cfd7e7cf
GersonRS <gersonrodriguessantos8@gmail.com>
2025-10-19 00:56:18 -0300

    refactor: refine PostgreSQL module outputs
    - Updated outputs.tf with refined output definitions
    - Improved module output structure and documentation

--- outputs.tf
+++ outputs.tf
@@ -9,7 +9,7 @@ output "credentials" {
 
 output "cluster_dns" {
   description = "Postgres cluster dns"
-  value       = "postgresql.postgresql.svc.cluster.local"
+  value       = "postgresql.${var.namespace}.svc.cluster.local"
 }
 output "cluster_ip" {
   description = "Postgres Cluster IPs"
